02eb0e5c03944cec7fb14776325915f810bf04f3
Dinar <sagitoff.dinar@yandex.ru>
2021-03-16 18:08:15 +0300

    коммит #2

--- script.js
+++ script.js
@@ -1,3 +1,52 @@
-let cells = document.querySelectorAll('#field td');
+const cells = document.querySelectorAll('#field td');
+const button = document.querySelector('.button');
 
-console.log(cells);
+button.addEventListener("click", () => window.location.reload());
+
+
+
+start(cells)
+
+function start(cells) {
+	let i = 0;
+	
+	for (let cell of cells) {
+		cell.addEventListener('click', function step() {
+			this.innerHTML = ['X', 'O'][i % 2];
+			this.removeEventListener('click', step);
+			
+			if (isVictory(cells)) {
+				alert(this.innerHTML);
+			} else if (i == 8) {
+				alert('ничья');
+			}
+			
+			i++;
+		});
+	}
+}
+
+function isVictory(cells) {
+	let combs = [
+		[0, 1, 2],
+		[3, 4, 5],
+		[6, 7, 8],
+		[0, 3, 6],
+		[1, 4, 7],
+		[2, 5, 8],
+		[0, 4, 8],
+		[2, 4, 6],
+	];
+
+	for (let comb of combs) {
+		if (
+			cells[comb[0]].innerHTML == cells[comb[1]].innerHTML &&
+			cells[comb[1]].innerHTML == cells[comb[2]].innerHTML &&
+			cells[comb[0]].innerHTML != ''
+		) {
+			return true;
+		}
+	}
+	
+	return false;
+}
